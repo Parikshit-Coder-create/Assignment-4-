27e2038586232ec3312fb032172dcec76731fbab
Parikshit-Coder-create <parikshitrathore1710@gmail.com>
2026-09-01 23:25:36 +0530

    Add Q7.c with increment and decrement examples

---
+++ Q7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+
+    printf("Initial = %d\n",a);
+
+    printf("++a = %d\n",++a);
+    printf("a++ = %d\n",a++);
+    printf("After a++ = %d\n",a);
+
+    printf("--a = %d\n",--a);
+    printf("a-- = %d\n",a--);
+    printf("After a-- = %d\n",a);
+
+    return 0;
+}
